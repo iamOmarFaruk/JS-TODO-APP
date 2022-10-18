4de32e4e00de0f37da4817cd7eb6d0c3e491c69a
iamOmarFaruk <omarbg.bd@gmail.com>
2022-10-18 21:04:49 +0600

    edit option added

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const renderTasks = (tasks) => {
         <td class="task-description">${task.description}</td>
         <td class="task-priority"><span class="badge badge-${priority}">${task.priority}</span></td>
         <td class="task-action">
-          <button class="btn btn-primary">Edit</button>
+          <button class="btn btn-primary" onclick="editTask(${task.id})" data-toggle="modal" data-target="#editTaskModal">Edit</button>
           <button type="submit" class="btn btn-danger delete-button" onclick="if(confirm('Are you sure?')) deleteTasks(${task.id})">Delete</button>
           
         </td>
@@ -45,6 +45,90 @@ function deleteTasks(id) {
   // re-render the list
   renderTasks(tasks);
 }
+
+// edit tasks
+const editTask = (id) => {
+  tasks.forEach((task) => {
+    if (task.id === id) {
+      // check if #editTaskModal is in the DOM then remove the element
+      if (document.querySelector("#editTaskModal")) {
+        document.querySelector("#editTaskModal").remove();
+      }
+
+      // cteate a modal for editing task description and priority
+      let editTaskModal = `
+  <div class="modal fade" id="editTaskModal" tabindex="-1" role="dialog" aria-labelledby="editTaskModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="editTaskModalLabel">Edit Task</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="edit-task-description">Task Description</label>
+            <input type="text" class="form-control" id="edit-task-description" placeholder="Enter Task Description" value=${task.description}>
+          </div>
+          <div class="form-group">
+            <label for="edit-task-priority">Task Priority</label>
+            <select class="form-control" id="edit-task-priority">
+              <option id="option_high">High</option>
+              <option id="option_medium">Medium</option>
+              <option id="option_low">Low</option>
+            </select>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" onclick="saveEditTask(${id})">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+
+      // append the modal to the body
+      document.body.insertAdjacentHTML("beforeend", editTaskModal);
+
+      // #edit-task-priority - select the option with the given priority
+      // if (task.priority === "High") {
+      //   document.querySelector("#option_high").selected = true;
+      // } else if (task.priority === "Medium") {
+      //   document.querySelector("#option_medium").selected = true;
+      // } else {
+      //   document.querySelector("#option_low").selected = true;
+      // } do the same thing using ternary operator
+      document.querySelector(
+        `#option_${task.priority.toLowerCase()}`
+      ).selected = true;
+    }
+  });
+};
+
+// save the edited task
+const saveEditTask = (id) => {
+  // get the edited task description and priority
+  let editedTaskDescription = document.querySelector(
+    "#edit-task-description"
+  ).value;
+  let editedTaskPriority = document.querySelector("#edit-task-priority").value;
+  // find the task with the given id and update the description and priority
+  tasks = tasks.map((task) => {
+    if (task.id === id) {
+      task.description = editedTaskDescription;
+      task.priority = editedTaskPriority;
+    }
+    return task;
+  });
+  // save the new tasks array to localStorage
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  // re-render the list
+  renderTasks(tasks);
+};
 
 // jquery ready
 window.addEventListener("DOMContentLoaded", () => {
